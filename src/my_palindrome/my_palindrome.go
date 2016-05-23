edb3feb258843470474e376784738292d6c7f2c3
Jeremy Udit <jeremy.udit@sheridanc.on.ca>
2016-05-23 19:15:46 -0400

    Replace UTF-8 recursive IsPalindrome() with looping function

--- src/my_palindrome/my_palindrome.go
+++ src/my_palindrome/my_palindrome.go
@@ -43,12 +43,15 @@ func init() {
 			if utf8.RuneCountInString(s) <= 1 {
 				return true
 			}
-			first, sizeOfFirst := utf8.DecodeRuneInString(s)
-			last, sizeOfLast := utf8.DecodeLastRuneInString(s)
-			if first != last {
-				return false
+			for temps := s; len(temps) > 1; {
+				first, sizeOfFirst := utf8.DecodeRuneInString(s)
+				last, sizeOfLast := utf8.DecodeLastRuneInString(s)
+				temps = temps[sizeOfFirst : len(temps)-sizeOfLast]
+				if first != last {
+					return false
+				}
 			}
-			return IsPalindrome(s[sizeOfFirst : len(s)-sizeOfLast])
+			return true
 		}
 	}
 }
